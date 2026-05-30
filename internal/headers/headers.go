9bf65d511e368abac523234411ef04e3986b2030
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2026-05-30 13:24:53 +0700

    fix: handle case insensitive

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,10 +3,14 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"regexp"
+	"slices"
 	"strings"
 )
 
 const crlf = "\r\n"
+
+var allowedChars = regexp.MustCompile(`^[A-Za-z0-9!#$%&'*+\-.\^_` + "`" + `|~]+$`)
 
 type Headers map[string]string
 
@@ -25,10 +29,14 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 	}
 
 	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
-	key := string(parts[0])
+	key := strings.ToLower(string(parts[0]))
 
 	if key != strings.TrimSpace(key) {
 		return 0, false, fmt.Errorf("invalid header name %s", key)
+	}
+
+	if !validTokens([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid header token found %s", key)
 	}
 
 	value := bytes.TrimSpace(parts[1])
@@ -39,5 +47,29 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 }
 
 func (h Headers) Set(key string, value string) {
+	key = strings.ToLower(key)
 	h[key] = value
+}
+
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
+func validTokens(data []byte) bool {
+	for _, c := range data {
+		if !isTokenChar(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isTokenChar(c byte) bool {
+	if c >= 'A' && c <= 'Z' ||
+		c >= 'a' && c <= 'z' ||
+		c >= '0' && c <= '9' {
+		return true
+	}
+
+	return slices.Contains(tokenChars, c)
 }
