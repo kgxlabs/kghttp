f82d8d4352ff2fe86784fc84f5afb95a0923503f
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2026-06-01 14:24:45 +0700

    refactor: move handler from inline to separate

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -13,30 +13,7 @@ import (
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port, server.Handler(func(w io.Writer, req *request.Request) *server.HandlerError {
-		if req.RequestLine.RequestTarget == "/yourproblem" {
-			return &server.HandlerError{
-				StatusCode: 400,
-				Message:    "Your problem is not my problem\n",
-			}
-		}
-
-		if req.RequestLine.RequestTarget == "/myproblem" {
-			return &server.HandlerError{
-				StatusCode: 500,
-				Message:    "Woopsie, my bad\n",
-			}
-		}
-
-		if _, err := w.Write([]byte("All good, frfr\n")); err != nil {
-			return &server.HandlerError{
-				StatusCode: 500,
-				Message:    "Internal Server Error",
-			}
-		}
-
-		return nil
-	}))
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -47,4 +24,29 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: 400,
+			Message:    "Your problem is not my problem\n",
+		}
+	}
+
+	if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: 500,
+			Message:    "Woopsie, my bad\n",
+		}
+	}
+
+	if _, err := w.Write([]byte("All good, frfr\n")); err != nil {
+		return &server.HandlerError{
+			StatusCode: 500,
+			Message:    "Internal Server Error",
+		}
+	}
+
+	return nil
 }
