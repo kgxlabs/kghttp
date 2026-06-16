8c719a74264bef347148890849ea465113b3c9f4
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2026-06-16 21:08:07 +0700

    feat: chunked outline

--- kghttp/internal/chunked.go
+++ kghttp/internal/chunked.go
@@ -1,6 +1,7 @@
 package kghttp
 
 import (
+	"bytes"
 	"errors"
 	"io"
 
@@ -12,7 +13,10 @@ var (
 )
 
 type chunkedReader struct {
-	r io.Reader
+	r          *kgbuf.Reader
+	n          int
+	chunkEnded bool
+	err        error
 }
 
 func NewChunkedReader(r io.Reader) io.Reader {
@@ -26,6 +30,59 @@ func NewChunkedReader(r io.Reader) io.Reader {
 	}
 }
 
-func (cr *chunkedReader) Read(p []byte) (int, error) {
-	return 0, nil
+func (cr *chunkedReader) beginChunkProcess() {
+	// TODO: come back here after these issues https://github.com/Kaung-HtetKyaw/kgx/issues/21 , https://github.com/Kaung-HtetKyaw/kgx/issues/22
+}
+
+func (cr *chunkedReader) chunkHeaderAvailable() bool {
+	n := cr.r.Buffered()
+
+	if n > 0 {
+		peek, _ := cr.r.Peek(n)
+		return bytes.Index(peek, []byte("\r\n")) > 0
+	}
+
+	return false
+}
+
+func (cr *chunkedReader) Read(p []byte) (n int, err error) {
+
+	for cr.err == nil {
+
+		if cr.n == 0 {
+			// do no wait for more data if next chunk header is not immediately available
+			if n > 0 && !cr.chunkHeaderAvailable() {
+				break
+			}
+
+			// begin processing chunks headers here
+			cr.beginChunkProcess()
+
+			// there could be malfomred chunked encoding so restart loop to recheck error
+			continue
+		}
+
+		if len(p) == 0 {
+			break
+		}
+
+		rbuf := p
+		if len(p) > cr.n {
+			rbuf = rbuf[:cr.n]
+		}
+
+		var bn int
+		bn, cr.err = cr.r.Read(rbuf)
+		n += bn
+		cr.n -= bn
+		p = p[bn:]
+
+		if cr.n == 0 && cr.err == nil {
+			cr.chunkEnded = true
+		} else if err == io.EOF {
+			cr.err = io.ErrUnexpectedEOF
+		}
+	}
+
+	return n, cr.err
 }
